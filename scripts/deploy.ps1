8bfe6857d58344b81ce14855620bdb8de479f61f
stephen lowery <slvt16@vt.edu>
2020-02-23 08:38:52 -0500

    that's my secret cap; i use powershell

---
+++ scripts/deploy.ps1
@@ -0,0 +1,5 @@
+rm ./get_reviews_lambda.zip;
+7z u ./get_reviews_lambda.zip ./get_reviews_lambda/lambda_function.py;
+7z u ./get_reviews_lambda.zip ./get_reviews_lambda/package/*;
+aws s3 cp ./get_reviews_lambda.zip s3://site-reviews/get_site_reviews.zip; 
+aws lambda update-function-code --function-name get_site_reviews --s3-bucket site-reviews --s3-key get_site_reviews.zip
