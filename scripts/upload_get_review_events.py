34bd17d0d10d62377239a459beb7b7fb1d022b8f
stephen lowery <slvt16@vt.edu>
2020-02-23 08:40:56 -0500

    script to kick off sqs/lambda

---
+++ scripts/upload_get_review_events.py
@@ -0,0 +1,20 @@
+"""
+Script to send events to SQS for review scraping
+"""
+
+import os
+
+import boto3
+
+SQS_ADDRESS = os.environ.get('SQS_ADDRESS')
+
+sqs = boto3.client('sqs')
+
+with open(os.path.join('scrape','companies.txt')) as f:
+    urls = f.read().split('\n')
+
+for url in urls:
+    sqs.send_message(
+        QueueUrl=SQS_ADDRESS,
+        MessageBody=url
+    )
