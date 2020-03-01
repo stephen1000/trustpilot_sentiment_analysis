e2de0cf8f1a586782892a38ef97848aeaa9c95cd
stephen lowery <slvt16@vt.edu>
2020-03-01 08:12:28 -0500

    changed alias to actual command

--- scripts/deploy.ps1
+++ scripts/deploy.ps1
@@ -1,4 +1,4 @@
-rm ./get_reviews_lambda.zip;
+Remove-Item ./get_reviews_lambda.zip;
 7z u ./get_reviews_lambda.zip ./get_reviews_lambda/lambda_function.py;
 7z u ./get_reviews_lambda.zip ./get_reviews_lambda/package/*;
 aws s3 cp ./get_reviews_lambda.zip s3://site-reviews/get_site_reviews.zip; 
