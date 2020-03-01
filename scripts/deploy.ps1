a4d94fe069a10d0fbe67e21a5c6d73e76ea6f55a
stephen lowery <slvt16@vt.edu>
2020-03-01 08:16:17 -0500

    also dump zip after upload

--- scripts/deploy.ps1
+++ scripts/deploy.ps1
@@ -3,3 +3,4 @@ Remove-Item ./get_reviews_lambda.zip;
 7z u ./get_reviews_lambda.zip ./get_reviews_lambda/package/*;
 aws s3 cp ./get_reviews_lambda.zip s3://site-reviews/get_site_reviews.zip; 
 aws lambda update-function-code --function-name get_site_reviews --s3-bucket site-reviews --s3-key get_site_reviews.zip
+Remove-Item ./get_reviews_lambda.zip;
